a7f545e5ce414b60f562b7116f4740a3adf1e35b
Rishi Sayanthakumar <160595074+Christumas@users.noreply.github.com>
2025-06-24 16:43:40 +0200

    Added a random set of trials just for testing purposes. There are some issues, mainly the responsiveness. Triangles, stars, circles, pacman shapes dont appear centered anymore. Need to find a fix for this

--- Testing Object Generation/script.js
+++ Testing Object Generation/script.js
@@ -1,6 +1,9 @@
 const jsPsych = initJsPsych();
 const timeline = []
 
+//the shapes will be drawn using the on_load parameter of the jsPsychHtmlKeyboardResponse plugin
+//the canvas plugin only lets us take one argument which is disadvantageous as we need control of the shape colour and texture
+
 
 // Find the size of the bounding box of the canvas element. This is essential to calculate the 
 // offset needed to ensure that the shape is centered. The size of the bounding box is essentially
@@ -37,7 +40,7 @@ function getBoundingBox(canvasWidth, canvasHeight, Paths){
 }
 
 
-function drawRect(){
+function drawRect(colour=null, texture=null){
     const canvas = document.querySelector("#jspsych-canvas-keyboard-response-stimulus")
     const context = canvas.getContext('2d')
     const canvas_width = canvas.clientWidth;
@@ -49,14 +52,38 @@ function drawRect(){
     const Xpos = (canvas_width/2) - (rectangleWidth/2) ;
     const Ypos = (canvas_height/2) - (rectangleHeight/2);
 
-    context.fillStyle = "red";
-    context.fillRect(Xpos,Ypos, rectangleWidth, rectangleHeight);
+    context.rect(Xpos,Ypos,rectangleWidth,rectangleHeight);
+    context.save()
+    context.clip()
+
+    if (texture){
+        const img = new Image();
+        img.src = texture;
+        img.onload = () => {
+            const pattern = context.createPattern(img,'repeat') 
+            context.fillStyle = pattern;
+            context.fill()
+
+            context.fillStyle = colour;
+            context.fill()
+            context.restore()
+
+            context.lineWidth = 3;
+            context.stroke()
+        }
+
+    }
+
+    else{
+        context.lineWidth = 2;
+        context.stroke()
+    }
     
 
 }
 
 
-function drawPlus(){    
+function drawPlus(colour=null, texture=null){    
     paths = [
         [0,0],
         [0, 25],
@@ -75,7 +102,7 @@ function drawPlus(){
     
     const canvas = document.querySelector("#jspsych-canvas-keyboard-response-stimulus")
     const context = canvas.getContext('2d')
-    context.fillStyle = "green";
+
     const canvas_width = canvas.clientWidth;
     const canvas_height = canvas.clientHeight;
 
@@ -93,11 +120,35 @@ function drawPlus(){
             context.lineTo(x,y)
         }
     })
-    context.fill()
+    context.save()
+    context.clip()
+
+    if (texture){
+        const img = new Image();
+        img.src = texture;
+        img.onload = () => {
+            const pattern = context.createPattern(img,'repeat') 
+            context.fillStyle = pattern;
+            context.fill()
+
+            context.fillStyle = colour;
+            context.fill()
+            context.restore()
+
+            context.lineWidth = 3;
+            context.stroke()
+        }
+
+    }
+
+    else{
+        context.lineWidth = 2;
+        context.stroke()
+    }
 
 }
 
-function drawTriangle(){
+function drawTriangle(colour=null, texture=null){
     
     const paths = [
         [0,0],
@@ -124,10 +175,35 @@ function drawTriangle(){
         }
     })
 
-    context.fill()
+    context.closePath()
+    context.save()
+    context.clip()
+
+    if (texture){
+        const img = new Image();
+        img.src = texture;
+        img.onload = () => {
+            const pattern = context.createPattern(img,'repeat') 
+            context.fillStyle = pattern;
+            context.fill()
+
+            context.fillStyle = colour;
+            context.fill()
+            context.restore()
+
+            context.lineWidth = 3;
+            context.stroke()
+        }
+
+    }
+
+    else{
+        context.lineWidth = 2;
+        context.stroke()
+    }
 }
 
-function drawPacman(){
+function drawPacman(colour=null, texture=null){
     const canvas = document.querySelector("#jspsych-canvas-keyboard-response-stimulus")
     const context = canvas.getContext('2d');
     const canvas_width = canvas.clientWidth;
@@ -141,9 +217,35 @@ function drawPacman(){
     context.closePath()
     context.fill()
 
+    context.save()
+    context.clip()
+
+    if (texture){
+        const img = new Image();
+        img.src = texture;
+        img.onload = () => {
+            const pattern = context.createPattern(img,'repeat') 
+            context.fillStyle = pattern;
+            context.fill()
+
+            context.fillStyle = colour;
+            context.fill()
+            context.restore()
+
+            context.lineWidth = 3;
+            context.stroke()
+        }
+
+    }
+
+    else{
+        context.lineWidth = 2;
+        context.stroke()
+    }
+
 }
 
-function drawCircle(){
+function drawCircle(colour=null, texture=null){
    
     const canvas = document.querySelector("#jspsych-canvas-keyboard-response-stimulus")
     const context = canvas.getContext('2d');
@@ -156,10 +258,39 @@ function drawCircle(){
     context.arc(canvasCenterX,canvasCenterY,100, 0, Math.PI*2, false)
     context.fill()
 
+    context.save()
+    context.clip()
+
+    if (texture){
+        const img = new Image();
+        img.src = texture;
+        img.onload = () => {
+            const pattern = context.createPattern(img,'repeat') 
+            context.fillStyle = pattern;
+            context.fill()
+
+            context.fillStyle = colour;
+            context.fill()
+            context.restore()
+
+            context.lineWidth = 3;
+            context.stroke()
+        }
+
+    }
+
+    else{
+        context.lineWidth = 2;
+        context.stroke()
+    }
+
 }
 
-function drawStar(spikes=5, outerRadius=100, innerRadius=50){
+function drawStar(colour=null, texture=null){
 
+    const spikes = 5 
+    const outerRadius = 100 
+    const innerRadius = 50
     const canvas = document.querySelector("#jspsych-canvas-keyboard-response-stimulus")
     const context = canvas.getContext('2d');
     const canvas_width = canvas.clientWidth;
@@ -172,30 +303,54 @@ function drawStar(spikes=5, outerRadius=100, innerRadius=50){
 
     context.beginPath();
     context.moveTo(centreX, centreY - outerRadius);
-    for (i=0; i<spikes; i++){
-        x = centreX + Math.cos(rotation)*outerRadius
-        y = centreY + Math.sin(rotation)*outerRadius
+    for (let i=0; i<spikes; i++){
+        var x = centreX + Math.cos(rotation)*outerRadius
+        var y = centreY + Math.sin(rotation)*outerRadius
         context.lineTo(x,y)
         rotation += step;
 
-        x = centreX + Math.cos(rotation)*innerRadius
-        y = centreY + Math.sin(rotation)*innerRadius
+        var x = centreX + Math.cos(rotation)*innerRadius
+        var y = centreY + Math.sin(rotation)*innerRadius
         context.lineTo(x,y)
         rotation += step;
     }
 
-    context.lineTo(centreX,centreY - outerRadius);
     context.closePath()
-    context.stroke()
 
     context.save()
     context.clip()
+    
+    if (texture){
+        console.log("texture is present")
+        const img = new Image();
+        img.src = texture;
+        img.onload = () => {
+            const pattern = context.createPattern(img,'repeat') 
+            context.fillStyle = pattern;
+            context.fill()
+
+            context.fillStyle = colour;
+            context.fill()
+            context.restore()
+
+            context.lineWidth = 3;
+            context.stroke()
+        }
+
+    }
+
+    else{
+        console.log("Texture not present")
+        context.lineWidth = 3
+        context.stroke();
+    }
 
 }
-function drawHexagon(){
+
+
+function drawHexagon(colour, texture){
     const canvas = document.querySelector("#jspsych-canvas-keyboard-response-stimulus")
     const context = canvas.getContext('2d');
-    
 
     const size = 100; //basically radius because the hexagon will be drawn using a circle as reference
     const sides = 6; //how many sides the polygon should have
@@ -212,21 +367,122 @@ function drawHexagon(){
         );
     }
 
-    context.stroke()
+    context.save()
+    context.clip()
+
+    if (texture){
+        const img = new Image();
+        img.src = texture;
+        img.onload = () => {
+            const pattern = context.createPattern(img,'repeat') 
+            context.fillStyle = pattern;
+            context.fill()
+
+            context.fillStyle = colour;
+            context.fill()
+            context.restore()
+
+            context.lineWidth = 3;
+            context.stroke()
+        }
+
+    }
+
+    else{
+        context.lineWidth = 2;
+        context.stroke()
+    }
+    
 }
 
 
+// const colore = "rgba(83, 233, 13, 0.65)"
+// const t1 = 'Textures/texture_dots.png'
 
+// let trial= {
+//     type: jsPsychHtmlKeyboardResponse,
+//     stimulus: `<div class="canvas-holder">
+//     <canvas id="jspsych-canvas-keyboard-response-stimulus" width=500 height=500 style="border:2px solid black;"></canvas>
+//     </div>`,
+//     on_load: function(){
+//         drawRect(colore,t1)
+//     },
+//     trial_duration: null
 
-let trial= {
-    type: jsPsychHtmlKeyboardResponse,
-    stimulus: `<div class="canvas-holder">
-    <canvas id="jspsych-canvas-keyboard-response-stimulus" width=500 height=500 style="border:2px solid black;"></canvas>
-    </div>`,
-    on_load: drawHexagon,
-    trial_duration: null
+// }
 
+const drawShapes = [
+    drawCircle,
+    drawHexagon,
+    drawPacman,
+    drawPlus,
+    drawRect,
+    drawStar,
+    drawTriangle
+]
+
+const colourVals = {
+    "Red" : "rgb(189, 38, 38,0.5)",
+    "Mustard Yellow" : "rgb(252, 181, 25,0.5)",
+    "Teal": "rgb(3, 135, 104, 0.5)",
+    "Green": "rgb(17, 199, 0,0.5)",
+    "Pink": "rgb(255, 3, 154, 0.5)",
+    "Azure": "rgb(3, 221, 255, 0.5)",
+    "Blue" : "rgb(66, 3, 255, 0.5)"
 }
 
-timeline.push(trial);
+
+const textures = [
+    "Textures\\texture_bottle.png",
+    "Textures\\texture_bricks.png",
+    "Textures\\texture_dots.png",
+    "Textures\\texture_flannel.png",
+    "Textures\\texture_grid.png",
+    "Textures\\texture_verticalLines.png",
+    "Textures\\texture_waves.png"
+]
+
+
+function generateTrials(functionArray, colourdict, textureArray){
+    const colours = Object.keys(colourdict)
+    const trials = []
+
+    if (functionArray.length == Object.keys(colourdict)["length"]){
+        
+        textureArray.forEach( (element, index) => {
+            let a = Math.floor(Math.random() * functionArray.length);
+            let b = Math.floor(Math.random() * functionArray.length);
+            let c = Math.floor(Math.random() * functionArray.length);
+
+            let colour = colourdict[colours[b]];
+            let texture = textures[c];
+            
+
+            let trial = {
+                type: jsPsychHtmlKeyboardResponse,
+                stimulus: `
+                <div class="canvas-holder">
+                <canvas id="jspsych-canvas-keyboard-response-stimulus" width=500 height=500 style="border:2px solid black;"></canvas>
+                </div>
+                `,
+                on_load: function(){
+                    drawShapes[a](colour,texture)
+                },
+                trial_duration: null
+            }
+
+            trials.push(trial)
+
+                    
+        })
+
+    }
+
+    return trials
+}
+
+
+const trials = generateTrials(drawShapes,colourVals,textures);
+timeline.push(...trials)
+
 jsPsych.run(timeline)
